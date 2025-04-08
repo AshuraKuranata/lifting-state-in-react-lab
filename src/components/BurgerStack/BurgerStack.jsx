e7baa75ca96d86a9b8fc9490f6b9ba8776467158
JohnNg-GA <john.q.ng@gmail.com>
2025-04-07 22:16:18 -0700

    Initial set up of Lab

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,5 @@
+const BurgerStack = (props) => {
+    return <ul>{/*Map through props.ingredients*/}</ul>
+}
+
+export default BurgerStack
---
+++ src/components/IngredientList/IngredientList.jsx
@@ -0,0 +1,5 @@
+const IngredientList = (props) => {
+    return <ul>{/*Map through props.ingredients*/}</ul>
+}
+
+export default IngredientList;
